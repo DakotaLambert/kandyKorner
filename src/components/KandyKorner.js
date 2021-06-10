568c7bcded8c562cb5baeee1b7c6d859176062d6
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-06-10 11:52:12 -0400

    intial page rendering for visual cues

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { NavBar } from "./nav/NavBar";
+
+export const KandyKorner = () => (
+  <>
+    <NavBar />
+    <h1>Hi there.</h1>
+  </>
+);
